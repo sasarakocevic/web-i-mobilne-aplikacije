ad745062101e3f1a915fc66881eab85cc225084f
Sasa Rakocevic <shasasha83@gmail.com>
2022-03-15 11:41:50 +0100

    zavrsena baza

--- hotel.sql
+++ hotel.sql
@@ -42,6 +42,119 @@ INSERT INTO `drzava` VALUES (1,'Crna Gora'),(2,'Bosna i  Hercegovina'),(3,'Srbij
 UNLOCK TABLES;
 
 --
+-- Table structure for table `gost`
+--
+
+DROP TABLE IF EXISTS `gost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `gost` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ime` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `prezime` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `datum_rodjenja` date DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `drzava_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `gost_drzava_fk_idx` (`drzava_id`),
+  CONSTRAINT `gost_drzava_fk` FOREIGN KEY (`drzava_id`) REFERENCES `drzava` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gost`
+--
+
+LOCK TABLES `gost` WRITE;
+/*!40000 ALTER TABLE `gost` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gost` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gost_rezervacija`
+--
+
+DROP TABLE IF EXISTS `gost_rezervacija`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `gost_rezervacija` (
+  `id` int(11) NOT NULL,
+  `gost_id` int(10) unsigned NOT NULL,
+  `rezervacija_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gost_rezervacija_unq` (`gost_id`,`rezervacija_id`),
+  KEY `gost_rezervacija_gost_fk_idx` (`gost_id`),
+  KEY `gost_rezervacija_rezervacija_fk_idx` (`rezervacija_id`),
+  CONSTRAINT `gost_rezervacija_gost_fk` FOREIGN KEY (`gost_id`) REFERENCES `gost` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `gost_rezervacija_rezervacija_fk` FOREIGN KEY (`rezervacija_id`) REFERENCES `rezervacija` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gost_rezervacija`
+--
+
+LOCK TABLES `gost_rezervacija` WRITE;
+/*!40000 ALTER TABLE `gost_rezervacija` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gost_rezervacija` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `popust`
+--
+
+DROP TABLE IF EXISTS `popust`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `popust` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `cijena` decimal(5,2) DEFAULT NULL,
+  `datum` date DEFAULT NULL,
+  `soba_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `popust_soba_fk_idx` (`soba_id`),
+  CONSTRAINT `popust_soba_fk` FOREIGN KEY (`soba_id`) REFERENCES `soba` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `popust`
+--
+
+LOCK TABLES `popust` WRITE;
+/*!40000 ALTER TABLE `popust` DISABLE KEYS */;
+/*!40000 ALTER TABLE `popust` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rezervacija`
+--
+
+DROP TABLE IF EXISTS `rezervacija`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `rezervacija` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `datum_od` date NOT NULL,
+  `datum_do` date NOT NULL,
+  `soba_id` int(10) unsigned NOT NULL,
+  `kontakt` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rezervacija_soba_fk_idx` (`soba_id`),
+  CONSTRAINT `rezervacija_soba_fk` FOREIGN KEY (`soba_id`) REFERENCES `soba` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rezervacija`
+--
+
+LOCK TABLES `rezervacija` WRITE;
+/*!40000 ALTER TABLE `rezervacija` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rezervacija` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `slika`
 --
 
@@ -51,8 +164,10 @@ DROP TABLE IF EXISTS `slika`;
 CREATE TABLE `slika` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `putanja` varchar(255) COLLATE utf8_bin NOT NULL,
-  `soba_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `soba_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `slika_soba_fk_idx` (`soba_id`),
+  CONSTRAINT `slika_soba_fk` FOREIGN KEY (`soba_id`) REFERENCES `soba` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -77,12 +192,12 @@ CREATE TABLE `soba` (
   `broj` varchar(45) COLLATE utf8_bin NOT NULL,
   `tip_sobe_id` int(10) unsigned NOT NULL,
   `sprat_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`,`tip_sobe_id`,`sprat_id`),
+  PRIMARY KEY (`id`),
   KEY `sprat_fk_idx` (`sprat_id`),
   KEY `tip_sobe_fk_idx` (`tip_sobe_id`),
   CONSTRAINT `sprat_fk` FOREIGN KEY (`sprat_id`) REFERENCES `sprat` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `tip_sobe_fk` FOREIGN KEY (`tip_sobe_id`) REFERENCES `tip_sobe` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,4 +272,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-07 19:06:51
+-- Dump completed on 2022-03-15 11:28:58
