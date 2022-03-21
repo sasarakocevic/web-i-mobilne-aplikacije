2ca9c2b364ad37eecc3a23bce658bfa50ebacc91
Sasa Rakocevic <shasasha83@gmail.com>
2022-03-21 19:31:37 +0100

    Inicijalizacija laravel

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+    'middleware' => 'api'
+], function ($router) {
+    Route::get('drzava', [\App\Http\Controllers\DrzavaController::class, 'index']);
+    Route::post('drzava', [\App\Http\Controllers\DrzavaController::class, 'store']);
+});
